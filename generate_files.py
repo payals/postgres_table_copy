27a472dea36c0c79ebf62abded39fd799f565d25
Payal Singh <payals1@umbc.edu>
2020-09-05 02:15:04 -0400

    acls and indexes

--- generate_files.py
+++ generate_files.py
@@ -12,6 +12,27 @@ def create_clone(table_name, pk, convert):
     result = src.substitute(d)
     print(result)
 
+def create_indexes(table_name):
+    try:
+        conn = psycopg2.connect("dbname=postgres")
+    except psycopg2.Error as e:
+        print("Failed to connect to the database: ", e.pgcode)
+
+    template = open('templates/trigger.sql')
+    src = Template(template.read())
+
+    query = "select replace(indexdef,'INDEX', 'INDEX CONCURRENTLY') from pg_indexes where tablename = '{}'".format(table_name)
+    cur = conn.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    template = open('templates/indexes.sql')
+    src = Template(template.read())
+
+    d = {'indexes': '\n'.join(list(str(row) for row in rows))}
+    result = src.substitute(d)
+    print(result)
+
+
 
 def create_trigger(table_name, pk):
     template = open('templates/trigger.sql')
@@ -23,18 +44,35 @@ def create_trigger(table_name, pk):
     print(result)
 
 
-def main():
+def grant_acl(table_name):
 
     try:
         conn = psycopg2.connect("dbname=postgres")
     except psycopg2.Error as e:
         print("Failed to connect to the database: ", e.pgcode)
 
+    query = "select 'GRANT ' || privilege_type || ' ON ' || table_name || ' TO ' || grantee || ';' from information_schema.role_table_grants where table_name = '{}' and grantee <> grantor;".format(table_name, table_name)
+
+    cur = conn.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    template = open('templates/acl.sql')
+    src = Template(template.read())
+ 
+    d = {'acl': '\n'.join(str(row) for row in rows)}
+    result = src.substitute(d)
+    print(result)
+
+
+def main():
+
     table_name = "test"
     pk = "id"
-    convert = ['first', 'second', 'third']
+    convert = ['col1', 'col2', 'col3']
     create_trigger(table_name, pk)
     create_clone(table_name, pk, convert)
+    grant_acl(table_name)
+    create_indexes(table_name)
 
 
 if __name__ == "__main__":
